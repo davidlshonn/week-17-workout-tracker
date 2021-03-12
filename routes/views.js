7818cc8030bb5d92245e6363e30c8607faef93bc
David Shonn <davidlshonn@icloud.com>
2021-03-12 17:54:41 +0000

    get requests implemneted and working

--- routes/views.js
+++ routes/views.js
@@ -1,7 +1,16 @@
 //require in express router and path
+const router = require("express").Router();
+const path = require("path");
 
 // router get request which returns the exercise html file when user visits /exercise
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
 
 // router get request which returns states.html when the user visits /stats
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
 
 //export router
+module.exports = router;
